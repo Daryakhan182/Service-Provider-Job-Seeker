dff59d8810bfe1e87368c77e3dfbe241c73dca22
Darya khan <daryakhan182@gmail.com>
2019-12-13 04:59:29 -0800

    home page is updated with post discrition

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,19 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  PostForm: FormGroup;
+  constructor(private PostBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.formInitializer();
+  }
+
+  formInitializer() {
+    this.PostForm = this.PostBuilder.group({
+      ItemName: [null, [Validators.required]],
+      itemModel: [null, [Validators.required]],
+      itemDiscription: [null, [Validators.required]]
+    });
+  }
 
 }
