74a861fe50ba18385dbe9acbbd599c963d16a94a
Darya khan <daryakhan182@gmail.com>
2019-12-13 09:18:41 -0800

    upload images/files option added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+
 import { FormBuilder, Validators } from '@angular/forms';
 import { FormGroup } from '@angular/forms';
+
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Http } from '@angular/http'
 
 @Component({
   selector: 'app-home',
@@ -9,11 +12,37 @@ import { FormGroup } from '@angular/forms';
 })
 export class HomePage {
 
+  imageURL: string
   PostForm: FormGroup;
-  constructor(private PostBuilder: FormBuilder) {}
 
+  constructor(private PostBuilder: FormBuilder,
+    public http: Http) { }
+
+  @ViewChild('filebtn', { static: false }) filebtn
+
+  uploadFile() {
+    this.filebtn.nativeElement.click()
+  }
   ngOnInit() {
     this.formInitializer();
+  }
+
+  fileChanged(event)
+  {
+      const files= event.target.files
+      const data= new FormData()
+      data.append('file',files[0])
+      data.append('UPLOADCARE_STORE','1')
+      data.append('UPLOADCARE_PUB_KEY','7d8f8ea6d46da886b778')
+      
+      this.http.post('https://upload.uploadcare.com/base/',data ).
+      subscribe(event =>
+        {
+            console.log(event)
+            this.imageURL= event.json().file
+        }
+        )
+  
   }
 
   formInitializer() {
