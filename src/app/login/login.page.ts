29bfb11422b9cae85145d4c612718395b232485e
Darya khan <daryakhan182@gmail.com>
2019-12-21 07:00:17 -0800

    login and register pages are populated

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { UserService } from './../sdk/custom/user.service';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  styleUrls: ['./login.page.scss']
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+              private userService: UserService) {}
+
+  loginForm: FormGroup;
+  loading = false;
 
   ngOnInit() {
+    this.formInitializer();
   }
 
+  save() {
+    const loginData = this.loginForm.value;
+    console.log('loginData', loginData);
+    // we need to send this data to our node.js server
+
+    this.userService.userLogin(loginData).subscribe(
+      data => {
+        console.log('got response from server', data);
+        this.loading = false;
+      },
+      error => {
+        this.loading = false;
+        console.log('error', error);
+      }
+    );
+  }
+  
+  formInitializer() {
+    this.loginForm = this.formBuilder.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required]]
+    });
+  }
 }
